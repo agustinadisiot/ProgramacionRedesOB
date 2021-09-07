72a64c22ff59471976a1f919efb6ad8b739b2c8c
Agustina <agustinadisiotvevo@gmail.com>
2021-09-06 21:18:48 -0300

    clase CliMenu y funcion showMenu

---
+++ Obligatorio1DisiotMonjardin/Client/CliMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    public static class CliMenu
+    {
+        public static void showMenu(Dictionary<string,Action> options, string message) {
+            Console.Clear();
+            Console.WriteLine(message);
+            for (int i = 0; i < options.Count; i++)
+            {
+                var option = options.ElementAt(i);
+                Console.WriteLine($"{i+1}.{option.Key}");
+            }
+            bool isCorrectNumber = false;
+            int input = 1;
+            while (!isCorrectNumber)
+            {
+                bool isANumber = int.TryParse(Console.ReadLine(), out input);
+                int min = 1;
+                int max = options.Count;
+                if (isANumber)
+                {
+                    bool inRange = input >= min && input <= max;
+                    if (inRange) isCorrectNumber = true;
+                }
+                if (!isCorrectNumber)
+                {
+                    Console.WriteLine($"Elija un numero entre {min} y {max}");
+                }
+            }
+            var selectedOption = options.ElementAt(input - 1);
+            var selectedAction = selectedOption.Value;
+            selectedAction();
+        }
+    }
+}
---
+++ Obligatorio1DisiotMonjardin/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Dictionary<string, Action> opciones = new Dictionary<string, Action>();
+            opciones.Add("ver", () => Console.WriteLine("opcion ver XD"));
+            opciones.Add("comprar", () => Console.WriteLine("no compre este juego"));
+            opciones.Add("eliminar", () => Console.WriteLine("seguro que lo quiere borrar"));
+            opciones.Add("reimprimir", () => CliMenu.showMenu(opciones, "menucito"));
+            opciones.Add("explota", () => Main(args));
+
+            CliMenu.showMenu(opciones, "Menuuuu");
+        }
+    }
+}
