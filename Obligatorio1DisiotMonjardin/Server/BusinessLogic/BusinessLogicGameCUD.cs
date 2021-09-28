c7421072f133bdeda647f27077c03d06355c9250
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-09-27 23:20:43 -0300

    delete fil path and modified game refactor

--- Obligatorio1DisiotMonjardin/Server/BusinessLogic/BusinessLogicGameCUD.cs
+++ Obligatorio1DisiotMonjardin/Server/BusinessLogic/BusinessLogicGameCUD.cs
@@ -1,11 +1,14 @@
-﻿using Common.Domain;
+﻿using Common;
+using Common.Domain;
 using Common.NetworkUtils.Interfaces;
 using Common.Protocol;
 using Common.Utils;
+using Server.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace Server.BusinessLogic
 {
@@ -37,7 +40,7 @@ namespace Server.BusinessLogic
         public string PublishGame(Game newGame, INetworkStreamHandler nwsh)
         {
             BusinessLogicUtils utils = BusinessLogicUtils.GetInstance();
-            VerifyGame(newGame); 
+            VerifyGame(newGame);
             List<Game> games = da.Games;
             lock (games)
             {
@@ -57,6 +60,10 @@ namespace Server.BusinessLogic
             if (!Validation.isValidTitle(newGame.Title))
                 throw new ServerError("Título no válido");
 
+            var gameWithSameTitle = da.Games.Find(i => (i.Title == newGame.Title) && (i.Id != newGame.Id));
+            if (gameWithSameTitle != null)
+                throw new TitleAlreadyExistsException();
+
             if (!Validation.isValidSynopsis(newGame.Synopsis))
                 throw new ServerError("Sinopsis no válida");
 
@@ -70,6 +77,18 @@ namespace Server.BusinessLogic
                 throw new ServerError("CoverPath no válido");
         }
 
+        public bool DeleteGame(int gameId)
+        {
+            List<Game> games = da.Games;
+            lock (games)
+            {
+                Game gameToDelete = games.Find(i => i.Id == gameId);
+                string pathToDelete = gameToDelete.CoverFilePath;
+                DeleteFile.DeleteFileInAnotherThread(pathToDelete);
+                return games.Remove(gameToDelete);
+            }
+        }
+
         public string ModifyGame(int gameToModId, Game modifiedGame)
         {
             BusinessLogicUtils utils = BusinessLogicUtils.GetInstance();
@@ -79,56 +98,55 @@ namespace Server.BusinessLogic
                 Game gameToMod = utils.GetGameById(gameToModId);
 
                 if (modifiedGame.Title != "")
-                {
-                    var gameWithSameTitle = games.Find(i => (i.Title == modifiedGame.Title) && (i.Id != gameToModId));
-                    if (gameWithSameTitle != null)
-                        throw new TitleAlreadyExistsException();
-                    if (!Validation.isValidTitle(modifiedGame.Title))
-                        throw new ServerError("Título no válido");
-                    gameToMod.Title = modifiedGame.Title;
-                }
+                    gameToMod.Title = ModifiedValidTitle(modifiedGame.Title, gameToModId, games);
 
                 if (modifiedGame.Synopsis != "")
-                {
-                    if (!Validation.isValidSynopsis(modifiedGame.Synopsis))
-                        throw new ServerError("Sinopsis no válida");
-                    gameToMod.Synopsis = modifiedGame.Synopsis;
-                }
+                    gameToMod.Synopsis = ModifiedValidSynopsis(modifiedGame.Synopsis);
+
                 if (modifiedGame.ESRBRating != Common.ESRBRating.EmptyESRB)
-                {
-                    if (!Validation.isValidESRBRating((int)modifiedGame.ESRBRating))
-                        throw new ServerError("Clasificación ESRB no válida");
-                    gameToMod.ESRBRating = modifiedGame.ESRBRating;
-                }
+                    gameToMod.ESRBRating = ModifiedValidESRBRating(modifiedGame.ESRBRating);
 
                 if (modifiedGame.Genre != "")
-                {
-                    if (!Validation.isValidGenre(modifiedGame.Genre))
-                        throw new ServerError("Genero no válido");
-                    gameToMod.Genre = modifiedGame.Genre;
-                }
+                    gameToMod.Genre = ModifedValidGenre(modifiedGame.Genre);
 
                 if (modifiedGame.CoverFilePath != null)
                 {
                     string pathToDelete = gameToMod.CoverFilePath;
                     gameToMod.CoverFilePath = modifiedGame.CoverFilePath;
-                    File.Delete(pathToDelete); // TODO capaz no tendria que ir en Steam TODO lock
+                    DeleteFile.DeleteFileInAnotherThread(pathToDelete);
                 }
                 return $"Se modificó el juego {gameToMod.Title} correctamente";
             }
         }
 
-        public bool DeleteGame(int gameId)
+
+        private string ModifiedValidTitle(string title, int gameToModId, List<Game> games)
         {
-            List<Game> games = da.Games;
-            lock (games)
-            {
-                Game gameToDelete = games.Find(i => i.Id == gameId);
-                string pathToDelete = gameToDelete.CoverFilePath;
-                File.Delete(pathToDelete); // TODO capaz no tendria que ir en Steam TODO lock
-                return games.Remove(gameToDelete);
-            }
+            var gameWithSameTitle = games.Find(i => (i.Title == title) && (i.Id != gameToModId));
+            if (gameWithSameTitle != null)
+                throw new TitleAlreadyExistsException();
+            if (!Validation.isValidTitle(title))
+                throw new ServerError("Título no válido");
+            return title;
+        }
+        private string ModifiedValidSynopsis(string synopsis)
+        {
+            if (!Validation.isValidSynopsis(synopsis))
+                throw new ServerError("Sinopsis no válida");
+            return synopsis;
+        }
+        private ESRBRating ModifiedValidESRBRating(ESRBRating eSRBRating)
+        {
+            if (!Validation.isValidESRBRating((int)eSRBRating))
+                throw new ServerError("Clasificación ESRB no válida");
+            return eSRBRating;
         }
 
+        private string ModifedValidGenre(string genre)
+        {
+            if (!Validation.isValidGenre(genre))
+                throw new ServerError("Genero no válido");
+            return genre;
+        }
     }
 }
---
+++ Obligatorio1DisiotMonjardin/Server/Utils/DeleteFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Server.Utils
+{
+    public static class DeleteFile
+    {
+        public static void DeleteFileInAnotherThread(string path)
+        {
+            Thread deletingFileThread = new Thread(() => DeleteMutexFile(path));
+            deletingFileThread.Start();
+
+        }
+
+        private static void DeleteMutexFile(string pathToDelete)
+        {
+            lock (pathToDelete)
+            {
+                File.Delete(pathToDelete);
+            }
+
+        }
+    }
+}
